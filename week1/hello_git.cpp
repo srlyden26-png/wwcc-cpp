bd3d45add70bdadbbfb7a22d8beaa98a38d450e7
srlyden26-png <srlyden26@gmail.com>
2026-06-12 21:31:53 +0000

    Start

--- week1/hello_git.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "My first Git-tracked C++ program" << std::endl;
-    return 0;
-}
--- week1/makefile_demo.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Print the required message
-    cout << "This program was compiled using a Makefile" << endl;
-
-    return 0;
-}
