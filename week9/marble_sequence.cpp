748bc7960d60dba0689b0c48deea9e4c6b62d4a2
srlyden26-png <srlyden26@gmail.com>
2026-06-12 01:59:09 +0000

    Skeleton

---
+++ week9/marble_sequence.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// Skeleton at least
+int getMarbleSequenceValue(int marbleNumber) {
+    return 0;
+}
+
+int main() {
+    int marbleNumber = 5;
+    int result = getMarbleSequence(marbleNumber);
+    
+    std::cout<< "Testing set"
